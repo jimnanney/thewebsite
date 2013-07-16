ccb2b4bf2437fa1adbbe852ad9adeaec40a0f3e1
Ben Lavender <bhuga@github.com>
2013-07-16 18:23:18 -0500

    going in a more redis-y direction

--- tweet.rb
+++ tweet.rb
@@ -1,76 +1,24 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'data_mapper'
 require 'pusher'
-require 'carrierwave'
-require 'carrierwave/datamapper'
 require 'ostruct'
 require './s3'
 require './rest-client'
 require './string_splitter'
 require 'meme_captain'
 
-# DataMapper
-DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{ Dir.pwd }/development.sqlite3"))
-DataMapper.auto_upgrade!
-
 # Pusher
 Pusher.url = ENV['PUSHER_URL']
 
-# CarrierWave
-CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  #   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  #   endpoint: ENV["ASSET_HOST"]
-  # }
-
-  # config.fog_directory = ENV["S3_BUCKET"]
-  # config.storage :fog
-
-  # if settings.environment == :production
-  #   config.storage :fog
-  # else
-  #   config.storage :file
-  # end
-end
-
-# Meme Uploader
-class MemeUploader < CarrierWave::Uploader::Base
-  # storage :fog
-
-  # def store_dir
-  #   "uploads/memes/#{ model.id }"
-  # end
-end
-
 # Tweet Model
-class Tweet
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :tweet_id, String, length: 255
-  property :text, Text
-  property :keyword, String, length: 255
-  property :url, String, length: 255
-
-  property :user_id, String, length: 255
-  property :user_nickname, String, length: 255
-  property :user_name, String, length: 255
-  property :user_image, String, length: 255
-
-  property :meme, String, length: 255
-
-  # mount_uploader :meme, MemeUploader
-
+class Tweet < OpenStruct
   # From Twitter
   def self.from_twitter(status)
-    first_or_create({tweet_id: status.attrs[:id_str]}).tap do |tweet|
+    new.tap do |tweet|
       tweet.text = status.attrs[:text]
 
-
       blob = tweet.getImage
+
       key = "#{tweet.tweet_id}.jpg"
       S3.upload blob, key, :content_type => 'application/jpg'
 
