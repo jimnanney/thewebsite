acddce5974d18863218562f0ba991da95df87fdb
Ben Lavender <bhuga@github.com>
2013-07-16 18:40:01 -0500

    Further along

--- tweet.rb
+++ tweet.rb
@@ -1,3 +1,4 @@
+require './.env' unless ENV['RACK_ENV'] == 'production'
 require 'rubygems'
 require 'bundler/setup'
 require 'pusher'
@@ -6,6 +7,7 @@ require './s3'
 require './rest-client'
 require './string_splitter'
 require 'meme_captain'
+require 'open-uri'
 
 # Pusher
 Pusher.url = ENV['PUSHER_URL']
@@ -17,28 +19,37 @@ class Tweet < OpenStruct
     new.tap do |tweet|
       tweet.text = status.attrs[:text]
 
-      blob = tweet.getImage
+      tweet.upload_image(tweet.get_image)
 
-      key = "#{tweet.tweet_id}.jpg"
-      S3.upload blob, key, :content_type => 'application/jpg'
+      #tweet.user_id       = status.user.attrs[:id_str]
+      #tweet.user_nickname = status.user.attrs[:screen_name]
+      #tweet.user_name     = status.user.attrs[:name]
+      #tweet.user_image    = status.user.profile_image_url if status.user.profile_image_url?
+      #tweet.meme          = key
 
-      tweet.user_id       = status.user.attrs[:id_str]
-      tweet.user_nickname = status.user.attrs[:screen_name]
-      tweet.user_name     = status.user.attrs[:name]
-      tweet.user_image    = status.user.profile_image_url if status.user.profile_image_url?
-      tweet.meme          = key
-
-      tweet.save!
       tweet.push
     end
   end
 
-  # Push Tweet
-  def push
-    Pusher['twitter'].trigger('tweet', self.to_json)
+  def redis
+    @redis ||= Redis.connect :url => ENV['REDISTOGO_URL']
   end
 
-  def getImage
+  # Push Tweet
+  def push
+    ret = Pusher['twitter'].trigger('tweet', self.to_json)
+    puts "pushed"
+    ret
+  end
+
+  def upload_image(blob)
+    key = "#{tweet_id}.jpg"
+    ret = S3.upload blob, key, :content_type => 'application/jpg'
+    puts "uploaded"
+    ret
+  end
+
+  def get_image
       text = self.text
 
       searcher = ImageSearcher.new()
@@ -60,6 +71,7 @@ class Tweet < OpenStruct
 
       memeText = splitter.no_hashes(text)
       i = MemeCaptain.meme_top_bottom(file, splitter.left(memeText), splitter.right(memeText))
+      puts "image created"
       i.to_blob
   end
 
@@ -68,7 +80,6 @@ class Tweet < OpenStruct
     hash = JSON.parse(IO.read('./tweet.json'))
 
     hash.keys.each do |k|
-      puts k
       hash[k.to_sym] = hash[k]
     end
 
@@ -76,5 +87,3 @@ class Tweet < OpenStruct
     Tweet.from_twitter tweet
   end
 end
-
-DataMapper.finalize
--- views/app.coffee
+++ views/app.coffee
@@ -1,6 +1,7 @@
 $ ->
-	pusher  = new Pusher('ae645a2445d2f72cf3d4')
-	channel = pusher.subscribe('twitter')
-	channel.bind 'tweet', (data) ->
-		console.log data
-		$("#tweets").prepend "<li class='meme'><figure><img src='http://onlyinnola.tw/meme.jpg:#{ data.text.replace(/#onlyinnola/,'').replace(/#/,'') }'><figcaption class='social'><ul><li><a href='TheLinkThing'>Reddit</a></li><li><a href='TWEETLINK'>The Original Tweet</a></li><li><a href='PERMALINK'>Link to this</a></li></ul></figcaption></figure></li>"
+  pusher  = new Pusher('ae645a2445d2f72cf3d4')
+  channel = pusher.subscribe('twitter')
+  channel.bind 'tweet', (data) ->
+    console.log data
+    window.lastData = data
+    $("#tweets").prepend "<li class='meme'><figure><img src='http://onlyinnola.tw/meme.jpg:#{ data.text.replace(/#onlyinnola/,'').replace(/#/,'') }'><figcaption class='social'><ul><li><a href='TheLinkThing'>Reddit</a></li><li><a href='TWEETLINK'>The Original Tweet</a></li><li><a href='PERMALINK'>Link to this</a></li></ul></figcaption></figure></li>"
